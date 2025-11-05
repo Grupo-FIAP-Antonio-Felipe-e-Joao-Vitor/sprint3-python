5a42f3ebe5530fa484d679346534be7d0a40ce56
Antonio <antoniojacintoa@gmail.com>
2025-11-05 12:58:00 -0300

    adicionando comentarios

--- src/dados/data.py
+++ src/dados/data.py
@@ -1,9 +1,13 @@
 import json
 
+
+# Função que lê informações de json
 def lerInfos (caminho):
     with open(caminho, "r", encoding="utf-8") as arquivo:
         return json.load(arquivo)
 
+
+# Função que escreve dados em um arquivo json
 def escreverInfos (caminho, data):
     with open(caminho, "w", encoding="utf-8") as arquivo:
         json.dump(data, arquivo, indent=4, ensure_ascii=False)
